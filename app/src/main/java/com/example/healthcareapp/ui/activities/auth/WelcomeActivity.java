5527fbf3d994bd34dc3ec6ec18f8de53339ab480
lancer2672 <JBkhanhtran@gmail.com>
2023-05-01 19:44:13 +0700

    create ui for welcome screen

---
+++ app/src/main/java/com/example/healthcareapp/ui/activities/auth/WelcomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.healthcareapp.ui.activities.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.healthcareapp.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
