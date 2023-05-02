973cc81521e16f4248fd5762c8ee745123ad722d
lancer2672 <JBkhanhtran@gmail.com>
2023-05-02 14:38:18 +0700

    add bottom_nav_menu + fix ui

--- app/src/main/java/com/example/healthcareapp/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/healthcareapp/ui/activities/MainActivity.java
@@ -1,16 +1,36 @@
 package com.example.healthcareapp.ui.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.healthcareapp.R;
+import com.example.healthcareapp.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.bottomNavBar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.menu_home:
+                    case R.id.menu_statistic:
+                    case R.id.menu_profile:
+                        break;
+                    case R.id.menu_report:
+                        break;
+                    default:
+                }
+                return false;
+            }
+        });
     }
 }
